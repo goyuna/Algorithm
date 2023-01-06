bb8135fcd074cb0a4d8aa74fe1c876cc124030aa
goyuna <66414424+goyuna@users.noreply.github.com>
2023-01-06 23:47:50 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -2,18 +2,13 @@ c = int(input())    # 테스트케이스 개수
 
 for _ in range(c):
     scores = list(map(int, input().split()))    # 학생 점수리스트
-    n = scores[0]   # 학생 수
-    sum = 0         # 성적 합계
-    cnt = 0        # 평균을 넘는 학생 수
-
-    # 평균 구하기
-    for i in scores[1:]:
-        sum += i
-    avg = sum/(len(scores)-1)
+    avg = sum(scores[1:])/scores[0] # 평균
+    cnt = 0   # 평균을 넘는 학생 수
 
     # 평균을 넘는 학생 수 구하기
     for i in scores[1:]:
         if i > avg:
             cnt += 1
-
-    print(f"{(cnt/(len(scores)-1)*100):.3f}%")  # 소수점 셋째 자리까지 출력
+    
+    rate = cnt/scores[0]*100
+    print(f"{rate:.3f}%")  # 소수점 셋째 자리까지 출력
