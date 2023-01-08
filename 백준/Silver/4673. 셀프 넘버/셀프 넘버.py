614743b1d951740771728af258a229773b8143c8
goyuna <66414424+goyuna@users.noreply.github.com>
2023-01-08 21:03:17 +0900

    [Silver V] Title: 셀프 넘버, Time: 44 ms, Memory: 31656 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -0,0 +1,16 @@
+def selfnumber():
+    num_list = set(range(1,  10001))
+    generated_num = set()
+
+    for i in range(1, 10001):
+        for j in str(i):
+            i += int(j)
+        generated_num.add(i)
+        
+    self_num = sorted(num_list - generated_num)
+    for i in self_num:
+        print(i)
+
+    return 0
+
+selfnumber()
