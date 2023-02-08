c2ce4bc1f7516a9b382fe339401a071e02ade65b
goyuna <66414424+goyuna@users.noreply.github.com>
2023-02-09 00:44:42 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,18 +1,6 @@
 word = input()
 c_alp = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-ans = 0
 
-for i in range(len(word)):
-    if word[0:2] in c_alp:
-        ans += 1
-        word = word[2:]
-    elif word[0:3] in c_alp:
-        ans += 1
-        word = word[3:]
-    elif word == '':
-        break
-    else:
-        ans += 1
-        word = word[1:]
-
-print(ans)
+for i in c_alp:
+    word = word.replace(i, '*')
+print(len(word))
